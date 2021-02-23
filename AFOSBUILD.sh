f252ad33f92b014737719e9a1b21aa9bd3228bfa
TCT Source Code TEAM <source@thecrackertechnology.com>
2021-02-23 13:44:14 -0300

    Nma 7.91 source

---
+++ AFOSBUILD.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./configure --with-libpcre=included --with-libpcap=included --with-libssh2=included --with-libdnet=included --with-liblua=included --with-liblinear=included  --with-ndiif --with-zenmap
+make
+make install
