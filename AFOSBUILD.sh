ea730d8f7cf3030b26292263b79b18ce4f742659
TCT Source Code TEAM <source@thecrackertechnology.com>
2021-02-25 18:06:26 -0300

    update build options

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./configure --with-libpcre=included --with-libpcap=included --with-libssh2=included --with-libdnet=included --with-liblua=included --with-liblinear=included  --with-ndiif --with-zenmap
+./configure --with-libpcre=included --with-libssh2=included --with-libdnet=included --with-liblua=included --with-liblinear=included  --with-ndiif --with-zenmap
 make
 make install
